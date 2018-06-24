6958ff9e61c926946df3feb8a088a972016c58b9
@camapblue <lequangdao.itm@gmail.com>
2018-06-24 11:45:36 +0700

    Bug fixing

--- Sources/SwipeMenuView.swift
+++ Sources/SwipeMenuView.swift
@@ -257,7 +257,6 @@ open class SwipeMenuView: UIView {
         if currentIndex != index {
             delegate?.swipeMenuView(self, willChangeIndexFrom: currentIndex, to: index)
         }
-        isJumping = true
         jumpingToIndex = index
 
         tabView.jump(to: index)
